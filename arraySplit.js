6f62769249b6614095715a2a66c4c274b502c3a5
sumon <sumon01723@gmail.com>
2023-07-18 16:47:41 +0600

    array

---
+++ arraySplit.js
@@ -0,0 +1,16 @@
+// const nums = [1, 2, 3, 4, 5, 6, 7, 8];
+// const part = nums.slice(2, 5);
+// console.log(part);
+// console.log(nums);
+
+// const nums = [1, 2, 3, 4, 5, 6, 7, 8];
+// const part = nums.splice(2, 3, 77, 88, 99, 500);
+// console.log(part);
+// console.log(nums);
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8];
+const part = nums.splice(2, 3, 77, 88, 99, 500);
+// console.log(part);
+// console.log(nums);
+const together = nums.join(",");
+console.log(together);
